f47263f1e0a95f884079acbac503996e601ebcc1
sunyang <1487148859@qq.com>
2023-08-20 15:32:10 +0800

    8-20 withoutXML

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+//#include <gtsam/geometry/Pose3.h>
+//#include <gtsam/geometry/Rot3.h>
+//#include <gtsam/inference/Symbol.h>
+//#include <gtsam/navigation/CombinedImuFactor.h>
+//#include <gtsam/navigation/GPSFactor.h>
+//#include <gtsam/navigation/ImuFactor.h>
+//#include <gtsam/nonlinear/ISAM2.h>
+//#include <gtsam/nonlinear/LevenbergMarquardtOptimizer.h>
+//#include <gtsam/nonlinear/Marginals.h>
+//#include <gtsam/nonlinear/NonlinearFactorGraph.h>
+//#include <gtsam/nonlinear/Values.h>
+//#include <gtsam/slam/BetweenFactor.h>
+//#include <gtsam/slam/PriorFactor.h>
+
+
+// #include <gtsam_unstable/nonlinear/IncrementalFixedLagSmoother.h>
+
+#include <ros/ros.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/search/impl/search.hpp>
+#include <pcl/range_image/range_image.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/common/common.h>
+#include <pcl/common/transforms.h>
+#include <pcl/registration/icp.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/filter.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/crop_box.h>
+#include <pcl_conversions/pcl_conversions.h>
+
+
+int main()
+{
+    return 0;
+}
