73562873309ecbaf8c93483e94747427f041efa0
liusy <liuusy1127@163.com>
2023-09-16 10:43:47 +0800

    imuPre adapt to optmap before merge to rebuild

--- src/app_mapping/mapping_manager.h
+++ src/app_mapping/mapping_manager.h
@@ -23,7 +23,8 @@ public:
     ImageProjection img_proj;
     FeatureExtraction ft_extr;
 //    DeadReckoning dead_reckoning;
-//    OPTMapping opt_mapping;
+    IMUPreintegration imu_pre;
+    OPTMapping opt_mapping;
 
     void CloudCallback(const BaseType& msg){
         const CloudTypeXYZIRT& cloud_data = *((CloudTypeXYZIRT*)&msg);
@@ -33,7 +34,8 @@ public:
     void GNSSINSCallback(const BaseType& msg){
         const GNSSINSType& gnssins_data = *((GNSSINSType*)&msg);
         img_proj.AddGNSSINSSData(gnssins_data);
-//        opt_mapping.AddGNSSINSData(gnssins_data);
+        opt_mapping.AddGNSSINSData(gnssins_data);
+        imu_pre.AddGNSSINSData(gnssins_data);
 //        dead_reckoning.AddGNSSINSSData(gnssins_data);
     }
 
@@ -44,18 +46,17 @@ public:
         //然后开启各个线程
         img_proj.Init(pubsub);
         EZLOG(INFO)<< "img_proj success!!"<<endl;
-
         ft_extr.Init(pubsub);
-//        dead_reckoning.Init(pubsub);
-//        opt_mapping.Init(pubsub);
         EZLOG(INFO)<< "ft_extr success!!"<<endl;
-//        opt_mapping.Init(pubsub);
+        opt_mapping.Init(pubsub);
         EZLOG(INFO)<< "opt_mapping success!!"<<endl;
-
+        imu_pre.Init(pubsub);
+        EZLOG(INFO)<< "imu_pre success!!"<<endl;
         //构建数据流关系
         img_proj.ft_extr_ptr = &ft_extr;
 //        img_proj.opt_mapping_ptr = &opt_mapping;
-//        ft_extr.opt_mapping_ptr = &opt_mapping;
+        ft_extr.opt_mapping_ptr = &opt_mapping;
+        opt_mapping.imu_pre_ptr = &imu_pre;
         //opt_mapping.im
     }
 };
