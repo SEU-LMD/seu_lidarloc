48ac3bebcb1884ce268a94fc2a10d1f0df7c4464
liusy <liuusy1127@163.com>
2023-10-26 11:28:41 +0800

    add env

---
+++ env/pcl-1.10.0/cmake-build-debug/tools/CMakeFiles/pcl_passthrough_filter.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/pcl_passthrough_filter"
+  "../bin/pcl_passthrough_filter.pdb"
+  "CMakeFiles/pcl_passthrough_filter.dir/passthrough_filter.cpp.o"
+  "CMakeFiles/pcl_passthrough_filter.dir/passthrough_filter.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pcl_passthrough_filter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
