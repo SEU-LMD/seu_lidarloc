e5f74e56717bb7ab3cfe82ff6752242098be4bf9
wang267063 <x2295089343@qq.com>
2023-11-17 14:24:07 +0800

    env sh jiaoben

---
+++ env/compile_pcl.sh
@@ -0,0 +1,26 @@
+dir_abs_path=$(pwd)
+rm -rf ${dir_abs_path}/pcl-1.10.0-install/
+mkdir -p ${dir_abs_path}/pcl-1.10.0-install/
+cd pcl-1.10.0/
+rm -rf ./build/
+mkdir build
+cd build
+cmake				-DCMAKE_BUILD_TYPE=Release \
+				-DCMAKE_INSTALL_PREFIX=${dir_abs_path}/pcl-1.10.0-install \
+				-DWITH_QT=FALSE \
+				-DWITH_LIBUSB=false \
+				-DWITH_PNG=FALSE \
+				-DWITH_QHULL=FALSE \
+				-DWITH_CUDA=FALSE \
+				-DWITH_VTK=FALSE \
+				-DWITH_OPENGL=FALSE \
+				-DWITH_OPENNI=FALSE \
+				-DWITH_OPENNI2=FALSE \
+				-DWITH_PCAP=FALSE\
+				-DWITH_ENSENSO=FALSE\
+				-DWITH_VTK=FALSE\
+				..
+make -j 8
+make install                           
+cp -r ${dir_abs_path}/pcl-1.10.0/mycmake/* ${dir_abs_path}/pcl-1.10.0-install/share/pcl-1.10/
+
