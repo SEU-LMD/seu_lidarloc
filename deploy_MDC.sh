0d61c13003b0eead0f55a4a60488ba6d488dceed
fyy <fengyyml@163.com>
2023-11-25 16:11:46 +0800

    add mdc script

---
+++ deploy_MDC.sh
@@ -0,0 +1,9 @@
+rm -r ./Remote_MDC
+mkdir Remote_MDC
+sshfs mdc@192.168.1.6:/home/mdc/seu_wxq/autodrive_mdc/seu_lidarloc ./Remote_MDC/
+cp -rf ./data_check_res ./Remote_MDC/
+cp -rf ./env ./Remote_MDC/
+cp -rf ./map_data ./Remote_MDC/
+cp -rf ./src ./Remote_MDC/
+sshpass -p 'mdcOs_123' ssh -tt mdc@192.168.1.6
+
---
+++ login_MDC.sh
@@ -0,0 +1,2 @@
+sshpass -p 'mdcOs_123' ssh -tt mdc@192.168.1.6
+
